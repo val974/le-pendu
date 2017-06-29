555d58a45c64c2efe1e3b7cad40261652cfb5489
Alexandre Callens <alekzxand@gmail.com>
2017-06-29 15:26:49 +0400

    debut

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-const mots = ["ECLIPSE", "JAVASCRIPT","MALHONNETE","PHILOSOPHE","MARCHER", "TOMATES","NOMBRES","FEUILLE","VOITURE","COVOITURAGE"]
-
+const mots = ["eclipse", "javascript","malhonnete","philosophe","marcher", "tomates","nombres","feuille","voiture","covoiturage"]
 let chance = 0;
+let choix ="";
+let valeurTimer = 0;
+
+
+
+//mise en place du timer en cliquant sur jouer
+function jouer (){
+	valeurTimer = 30;
+	actualiserTimer();
+}
+function actualiserTimer(){
+	if (valeurTimer > 0)
+	{
+		valeurTimer = valeurTimer -1;
+		document.getElementById('afficherTimer')
+		.innerHTML = valeurTimer;
+    if (valeurTimer > 0)
+    { 
+    	monTimer = setTimeout('actualiserTimer()', 1000);
+
+    	}
+	}
+
 
 
 
@@ -8,18 +30,23 @@ let chance = 0;
 
 
 let motsOrdi = mots[Math.random() * mots.length | 0]
- alert(motsOrdi)
+ console.log(motsOrdi);
 		for(i=0; motsOrdi.length > i; i++){
-		      let choix = motsOrdi[i];
-		      console.log(choix)
+		       choix = motsOrdi[i];
+		      console.log(choix.charAt());
 		  }
-   let erreur = true;
-   		while(erreur){
-   			lettreUtilisateur =prompt("Devinez la lettre");
+console.log(motsOrdi)
+	let SiChoixOk = true;
+		while(SiChoixOk){
+   			let	lettreUtilisateur = prompt("Devinez la lettre");
+		    	if (lettreUtilisateur === choix.charAt()){ 
+		     			SiChoixOk = false;
+		     	
+		 }else{
+		 	chance++;
+		 }
+		} 
+		     	
+		}    	
 
-   		}
-
-		     if (lettreUtilisateur === choix[i]){
-		     	erreur = false;
-
-		 }  
+		 alert(chance)
